aa136ae4e3f5a139a72bb225128449e8e5f74457
Holly <snowflake@Hollys-MacBook-Pro.local>
2019-07-06 09:55:43 -0400

    updating model rpute

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const axios = require("axios");
-const db = require("../models");
+const db = require("../models/Book");
 const path = require("path");
 
 module.exports = function(app) {
